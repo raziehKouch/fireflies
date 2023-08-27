d843cbcdaf8cb912df747f97031b135db36807f2
Razieh Kouch <razieh.kouchakzadeh24@gmail.com>
2023-08-28 03:07:55 +0430

    تست اینکه تاگل فونتش عوض میشه یا نه

--- js/widgets.js
+++ js/widgets.js
@@ -56,6 +56,7 @@ Widgets.toggle = function(config){
 	var label = document.createElement("div");
 	label.id = "label";
 	label.innerHTML = config.label;
+	label.fontSize = 10;
 	dom.appendChild(label);
 
 	// Toggle
